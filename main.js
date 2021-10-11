152134ba3a2c1fd283470d8e1659c27077134e26
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-10-11 17:17:06 +0900

    call method to run game

--- main.js
+++ main.js
@@ -6,18 +6,37 @@ const fieldCharacter = '░';
 const pathCharacter = '*';
 
 class Field {
-  constructor(field) {
+  constructor(field = [[]]) {
     this.field = field;
     this.locationX = 0;
     this.locationY = 0;
+    // Set the "home" position before the game starts
     this.field[0][0] = pathCharacter;
   }
-  print() {
-    const displayString = this.field.map(row => {
-      return row.join('');
-    }).join('/n');
-    console.log(displayString);
+
+  runGame() {
+    let playing = true;
+    while (playing) {
+      this.print();
+      this.askQuestion();
+      if (!this.isInBounds()) {
+        console.log('Out of bounds instruction!');
+        playing = false;
+        break;
+      } else if (this.isHole()) {
+        console.log('Sorry, you fell down a hole!');
+        playing = false;
+        break;
+      } else if (this.isHat()) {
+        console.log('Congrats, you found your hat!');
+        playing = false;
+        break;
+      }
+      // Update the current location on the map
+      this.field[this.locationY][this.locationX] = pathCharacter;
+    }
   }
+
   askQuestion() {
     const answer = prompt('Which way? ').toUpperCase();
     switch (answer) {
@@ -34,11 +53,12 @@ class Field {
         this.locationX += 1;
         break;
       default:
-        console.log('Enter U, D, L, or R');
+        console.log('Enter U, D, L or R.');
         this.askQuestion();
         break;
     }
   }
+
   isInBounds() {
     return (
       this.locationY >= 0 &&
@@ -47,12 +67,22 @@ class Field {
       this.locationX < this.field[0].length
     );
   }
+
   isHat() {
     return this.field[this.locationY][this.locationX] === hat;
   }
+
   isHole() {
     return this.field[this.locationY][this.locationX] === hole;
   }
+
+  print() {
+    const displayString = this.field.map(row => {
+        return row.join('');
+      }).join('\n');
+    console.log(displayString);
+  }
+
   static generateField(height, width, percentage = 0.1) {
     const field = new Array(height).fill(0).map(el => new Array(width));
     for (let y = 0; y < height; y++) {
@@ -61,36 +91,20 @@ class Field {
         field[y][x] = prob > percentage ? fieldCharacter : hole;
       }
     }
+    // Set the "hat" location
     const hatLocation = {
       x: Math.floor(Math.random() * width),
       y: Math.floor(Math.random() * height)
     };
+    // Make sure the "hat" is not at the starting point
     while (hatLocation.x === 0 && hatLocation.y === 0) {
       hatLocation.x = Math.floor(Math.random() * width);
-      hatLocatiom.y = Math.floor(Math.random() * height);
+      hatLocation.y = Math.floor(Math.random() * height);
     }
-    field[hatLocation.y][hat.Location.x] = hat;
+    field[hatLocation.y][hatLocation.x] = hat;
     return field;
   }
-  runGame() {
-    let playing = true;
-    while (playing) {
-      this.print();
-      this.askQuestion();
-      if (!this.isInBounds()) {
-        console.log('Out of bounds');
-        playing = false;
-        break;
-      } else if (this.isHole()) {
-        console.log('Oops, you fell down a hole');
-        playing = false;
-        break;
-      } else if (this.isHat()) {
-        console.log('You found you hat, congratulations!!');
-        playing = false;
-        break;
-      }
-      this.field[this.locationY][this.locationX] = pathCharacter;
-    }
-  }
 }
+
+const myfield = new Field(Field.generateField(10, 10, 0.2));
+myfield.runGame();
